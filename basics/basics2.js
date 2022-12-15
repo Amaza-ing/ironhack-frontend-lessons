4195292d1d273876f06b7748b0a58be690c74261
Adrián Maza <adri_mv1@hotmail.com>
2022-12-15 12:15:03 +0100

    basics2

--- basics/basics2.js
+++ basics/basics2.js
@@ -71,3 +71,111 @@ console.log(sum(1, 2));
 
 const msgToUpperCase = msg => msg.toUpperCase();
 console.log(msgToUpperCase("hola mundo"));
+
+
+
+// add a key value pair to an object
+
+const myObject = {};
+console.log("myObject: ", myObject);
+myObject.key = "value";
+myObject["anotherKey"] = "another value";
+console.log("myObject modified: ", myObject);
+
+
+// array methods: map
+const numArray = [1, 2, 3, 28, 13, 4, 5, 10, 20];
+console.log("num array: ", numArray);
+
+const modifiedArray = numArray.map((number) => number * 2);
+console.log("modified array: ", modifiedArray);
+
+const countries = ["Spain", "Argentina", "Venezuela"];
+console.log(countries);
+const upperCaseCountries = countries.map((country) => country.toUpperCase());
+console.log(upperCaseCountries);
+
+const person1 = {
+  name: "Alberto",
+  age: 30,
+  city: "Madrid"
+}
+
+const person2 = {
+  name: "Beatriz",
+  age: 25,
+  city: "Barcelona"
+}
+
+const person3 = {
+  name: "Carmen",
+  age: 20,
+  city: "Málaga"
+}
+
+const people = [person1, person2, person3];
+console.log(people);
+
+const ages = people.map((person) => person.age);
+console.log(ages);
+
+const olderPeople = people.map((person) => {
+  person.age *= 2;
+  return person;
+})
+console.log(olderPeople);
+
+
+
+// array methods: filter
+
+const evenNumbers = numArray.filter((number) => (number % 2) === 0);
+console.log(evenNumbers);
+
+
+
+// array methods: reduce
+
+const sumResult = numArray.reduce((accum, currentValue) => {
+  return accum + currentValue;
+}, 0)
+console.log("sum result: ", sumResult);
+
+const countriesConcat = countries.reduce((accum, currentValue) => {
+  return accum + " " + currentValue;
+}, "")
+console.log("countries concat: ", countriesConcat);
+
+const countriesObject = countries.reduce((accum, currentValue) => {
+  accum[currentValue] = currentValue.length;
+  return accum;
+}, {})
+console.log("countries object: ", countriesObject);
+
+
+
+// array methods: sort
+const sortedCountries = [...countries];
+console.log(sortedCountries.sort());
+console.log(countries);
+
+// console.log(numArray.sort()); // this modifies the original array and sort it alphabetically
+
+const sortedNumbers = [...numArray];
+sortedNumbers.sort((a, b) => a - b);
+console.log("sorted numbers: ", sortedNumbers);
+
+const countriesByLength = [...countries].sort((a, b) => {
+  return b.length - a.length;
+})
+console.log(countriesByLength);
+
+const peopleByAge = [...people].sort((a, b) => {
+  return b.age - a.age;
+})
+console.log(peopleByAge);
+
+
+// array methods: reverse
+const reversedNumbers = [...numArray].reverse();
+console.log(reversedNumbers);
