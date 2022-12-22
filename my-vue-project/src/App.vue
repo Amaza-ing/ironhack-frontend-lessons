4684bc5e49542d19f01b217759783672bdfe274d
Adrián Maza <adri_mv1@hotmail.com>
2022-12-22 10:42:34 +0100

    pokemon api

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,59 +1,77 @@
 <template>
-  <FormComponent></FormComponent>
-  <BlogPost :title="myTrip">
-    <ul>
-      <li><p>{{ myNum }} Sitios que visité</p></li>
-      <li><p>Comida que probé</p></li>
-      <li><p>Fotos que saqué</p></li>
-    </ul>
-  </BlogPost>
-  <BlogPost title="Recetas deliciosas">
-    <template v-slot:firstWords>
-      <p>ñam ñam</p>
-    </template>
-    <template v-slot:endWords>
-      <p>Disfruta tu comida! ;)</p>
-    </template>
-  </BlogPost>
-  <BlogPost title="Mis películas favoritas"></BlogPost>
-  <BlogPost v-if="showPost" title="Hola a todos"></BlogPost>
-  <BlogPost v-else-if="showOtherPost" title="Adios a todos"></BlogPost>
-  <BlogPost v-else title="post de reserva"></BlogPost>
-
-  <h2 v-show="false">Me renderizo aunque no me muestre</h2>
-
-
-  <ul class="post-list">
-    <li v-for="(post, index) in posts" :key="post.id">
-      <h2>post id: {{ post.id }} & index: {{ index }}</h2>
-      <BlogPost :title="post.title"></BlogPost>
+  <ul class="pokemon-list">
+    <li v-for="(pokemon, index) in pokemons" :key="pokemon.name">
+      <PokemonCard :pokemon="pokemons[index]"></PokemonCard>
     </li>
   </ul>
 
 
 
-  <h1>This is inside App.vue</h1>
-  <p v-bind:class="myClass" :id="myId">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, doloribus!</p>
-  <TestComponent></TestComponent>
-  <p>This is my string: {{ myStr }} and this is my  number: {{ myNum }}</p>
-  <p v-if="myNum">This paragraph will show if myNum !== 0</p>
-  <p v-html="myHtml"></p>
-  <h3 v-on:click="myNum++">click me to add 1 to my number</h3>
-  <h3 @click="sayHello()">say hello</h3>
-  <h3 @click="resetMyNum()">reset number</h3>
-  <h3 @click="modifyMyClass()">modify myClass</h3>
+
+
+
+
+
+
+  <div v-if="false">
+    <FormComponent></FormComponent>
+    <BlogPost :title="myTrip">
+      <ul>
+        <li><p>{{ myNum }} Sitios que visité</p></li>
+        <li><p>Comida que probé</p></li>
+        <li><p>Fotos que saqué</p></li>
+      </ul>
+    </BlogPost>
+    <BlogPost title="Recetas deliciosas">
+      <template v-slot:firstWords>
+        <p>ñam ñam</p>
+      </template>
+      <template v-slot:endWords>
+        <p>Disfruta tu comida! ;)</p>
+      </template>
+    </BlogPost>
+    <BlogPost title="Mis películas favoritas"></BlogPost>
+    <BlogPost v-if="showPost" title="Hola a todos"></BlogPost>
+    <BlogPost v-else-if="showOtherPost" title="Adios a todos"></BlogPost>
+    <BlogPost v-else title="post de reserva"></BlogPost>
+  
+    <h2 v-show="false">Me renderizo aunque no me muestre</h2>
+  
+  
+    <ul class="post-list">
+      <li v-for="(post, index) in posts" :key="post.id">
+        <h2>post id: {{ post.id }} & index: {{ index }}</h2>
+        <BlogPost :title="post.title"></BlogPost>
+      </li>
+    </ul>
+  
+  
+  
+    <h1>This is inside App.vue</h1>
+    <p v-bind:class="myClass" :id="myId">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, doloribus!</p>
+    <TestComponent></TestComponent>
+    <p>This is my string: {{ myStr }} and this is my  number: {{ myNum }}</p>
+    <p v-if="myNum">This paragraph will show if myNum !== 0</p>
+    <p v-html="myHtml"></p>
+    <h3 v-on:click="myNum++">click me to add 1 to my number</h3>
+    <h3 @click="sayHello()">say hello</h3>
+    <h3 @click="resetMyNum()">reset number</h3>
+    <h3 @click="modifyMyClass()">modify myClass</h3>
+  </div>
 </template>
 
 <script>
 import TestComponent from "./components/TestComponent.vue"
 import BlogPost from "./components/BlogPost.vue"
 import FormComponent from "./components/FormComponent.vue"
+import PokemonCard from "./components/PokemonCard.vue"
 
 export default {
   components: {
     TestComponent,
     BlogPost,
-    FormComponent
+    FormComponent,
+    PokemonCard
   },
   data() {
     return {
@@ -70,7 +88,21 @@ export default {
         {title: "Recetas deliciosas", id: 1},
         {title: "Mis películas favoritas", id: 2},
         {title: "Hola a todos", id: 3}
-      ]
+      ],
+      pokemons: []
+    }
+  },
+  async created() {
+    for (let i = 1; i < 10; i++) {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      const data = await response.json()
+      const pokemon = {
+        name: data.forms[0].name,
+        hp: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        img: data.sprites.front_default
+      }
+      this.pokemons.push(pokemon)
     }
   },
   methods: {
@@ -108,5 +140,12 @@ p {
 .post-list {
   list-style: none;
   padding-left: 0;
+}
+
+.pokemon-list {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3px
 }
 </style>
---
+++ my-vue-project/src/components/PokemonCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="pokemon-card">
+    <div class="pokemon-name">
+      {{ pokemon.name }}
+    </div>
+    <div>
+      <img class="pokemon-img" :src="pokemon.img" alt="pokemon picture">
+    </div>
+    <div>
+      <span>HP: {{ pokemon.hp }} / </span>
+      <span>Attack: {{ pokemon.attack }}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: {
+      type: Object,
+      // required: true,
+      default: {
+        name: "Patata",
+        hp: "0",
+        attack: "0",
+        img: "https://pbs.twimg.com/profile_images/585913364720840704/Ta56Yrim_400x400.png"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .pokemon-card {
+    background-color: aliceblue;
+    width: 150px;
+    border: 2px solid blueviolet;
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  .pokemon-name {
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+  .pokemon-img {
+    max-width: 100px;
+  }
+</style>
